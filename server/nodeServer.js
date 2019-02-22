bcfb59c9589015900d109ab1acd81ced26dc58e1
Quinn <quinnluck17@gmail.com>
2019-02-21 23:44:25 -0700

    worked through w3 schools tutorial, I think I can stand on my own now

--- server/nodeServer.js
+++ server/nodeServer.js
@@ -1,13 +1,21 @@
 var http = require('http')
 var date = require('./modules/DateTime')
 var url = require('url')
+var fs = require('fs')
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("\n the date and time is currently: " + date.myDateTime())
-    res.write('\n' + req.url);
-    var q = url.parse(req.url, true).query;
-    var text = q.year + " " + q.month;
-    res.write("\n" + text);
-    res.end('\nHello World!');
+//    res.write("\n the date and time is currently: " + date.myDateTime())
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    res.write(filename)
+    fs.readFile(filename, function(err, data) {
+        if(err) {
+//            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 File not found");
+        }
+//        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data);
+        return res.end();
+    });
 }).listen(8080);
