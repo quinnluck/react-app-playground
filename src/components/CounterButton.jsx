fb6bf863a31d28bdbd84999722c7840923e9912f
Quinn Luck <quinnluck17@gmail.com>
2021-03-30 22:20:45 -0600

    started Pluralsight video on basics of react.  created shell for CounterButton

---
+++ src/components/CounterButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function CounterButton() {
+    return <button>TEST</button>
+}
+
+export default CounterButton
+//ReactDOM.render(<CounterButton/>, document.getElementById('mountNode'));
